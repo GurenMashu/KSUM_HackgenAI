ae2febbce77899e7c10668803a751d1f4696fa60
joeloommen <joelvo9694@gmail.com>
2025-07-19 22:34:21 +0530

    prompt added in

--- PromptEngine.py
+++ PromptEngine.py
@@ -15,5 +15,10 @@ class PromptEngine:
         self.model = model
         self.tokenizer = tokenizer
     def extract_keyword(self, prompt):
-        self.user_prompt = "find potential keywords to search for news articles and relevant articles that can be found in the internet from the following text: " + prompt
-        
+        self.user_prompt = prompt
+        self.system_prompt = "find potential keywords to search for news articles and relevant articles that can be found in the internet from the following text: "
+        self.generated_text = llm.generate_response(llm_model=self.model, llm_tokenizer=self.tokenizer, prompt_user=self.user_prompt, prompt_system=self.system_prompt)
+        print(self.generated_text)
+
+prompt_engine = PromptEngine(model=model, tokenizer=tokenizer)
+prompt_engine.extract_keyword(prompt="Hello, how are you my good man?")
--- llm.py
+++ llm.py
@@ -39,9 +39,9 @@ B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
 B_INST, E_INST = "[INST]", "[/INST]"
 
 
-def generate_response(prompt_user, prompt_system=SYSTEM_PROMPT, temperature=0.4):
+def generate_response(llm_model, llm_tokenizer, prompt_user, prompt_system=SYSTEM_PROMPT, temperature=0.4):
     prompt = f"{B_INST} {B_SYS}{prompt_system.strip()}{E_SYS}{prompt_user.strip()} {E_INST}\n\n"
-    pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
+    pipe = pipeline("text-generation", model=llm_model, tokenizer=llm_tokenizer)
     output = pipe(prompt, max_new_tokens=100, do_sample=True, temperature=0.5)
     full_text = output[0]["generated_text"]
     generated_text = full_text[len(prompt):].strip()
